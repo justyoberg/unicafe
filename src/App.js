715803a1a0c3a06e961a29fe4ddbfb1de7a9c57e
Justy Oberg <justyoberg1@gmail.com>
2023-03-26 16:42:19 -0700

    Finish exercise 1.10

--- src/App.js
+++ src/App.js
@@ -8,8 +8,23 @@ const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>
 }
 
-const Statistics = ({ text, calculation }) => {
-  return <p>{text}: {calculation} {text === 'Positive' ? '%' : ''}</p>
+const Statistics = ({ values }) => {
+  return values.all === 0 ? (
+    <p>No feedback given</p> 
+  ) : (
+    <div>
+      <StatisticLine text='Good' value={values.good} />
+      <StatisticLine text='Neutral' value={values.neutral} />
+      <StatisticLine text='Bad' value={values.bad} />
+      <StatisticLine text='All' value={values.all} />
+      <StatisticLine text='Average' value={values.avg} />
+      <StatisticLine text='Positive' value={values.percent} />
+    </div>
+  )
+}
+
+const StatisticLine = ({ text, value }) => {
+  return <p>{text}: {value} {text === 'Positive' ? '%' : ''}</p>
 }
 
 const App = () => {
@@ -19,6 +34,14 @@ const App = () => {
   const all = good + bad + neutral
   const avg = (good - bad) / all || 0
   const percent = (good / all) * 100 || 0
+  const values = {
+    good: good,
+    neutral: neutral,
+    bad: bad,
+    all: all,
+    avg: avg,
+    percent: percent
+  }
 
   return (
     <div>
@@ -27,18 +50,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral'/>
       <Button handleClick={() => setBad(bad + 1)} text='bad'/>
       <Header text='statistics' />
-      {all === 0 ? (
-        <p>No feedback given</p>
-      ) : (
-        <div>
-          <Statistics text='Good' calculation={good} />
-          <Statistics text='Neutral' calculation={neutral} />
-          <Statistics text='Bad' calculation={bad} />
-          <Statistics text='All' calculation={all} />
-          <Statistics text='Average' calculation={avg} />
-          <Statistics text='Positive' calculation={percent} />
-        </div>
-      )}
+      <Statistics values={values} />
     </div>
   )
 }
