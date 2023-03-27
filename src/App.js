a77d00962a7532a1689f6eddfd12b470e2cf9177
Justy Oberg <justyoberg1@gmail.com>
2023-03-26 18:38:26 -0700

    Finish exercise 1.11

--- src/App.js
+++ src/App.js
@@ -8,23 +8,30 @@ const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>
 }
 
-const Statistics = ({ values }) => {
+const Statistics = (values) => {
   return values.all === 0 ? (
     <p>No feedback given</p> 
   ) : (
-    <div>
-      <StatisticLine text='Good' value={values.good} />
-      <StatisticLine text='Neutral' value={values.neutral} />
-      <StatisticLine text='Bad' value={values.bad} />
-      <StatisticLine text='All' value={values.all} />
-      <StatisticLine text='Average' value={values.avg} />
-      <StatisticLine text='Positive' value={values.percent} />
-    </div>
+      <table>
+        <tbody>
+          <StatisticLine text='Good' value={values.good} />
+          <StatisticLine text='Neutral' value={values.neutral} />
+          <StatisticLine text='Bad' value={values.bad} />
+          <StatisticLine text='All' value={values.all} />
+          <StatisticLine text='Average' value={values.avg} />
+          <StatisticLine text='Positive' value={values.percent} />
+        </tbody>
+      </table>
   )
 }
 
 const StatisticLine = ({ text, value }) => {
-  return <p>{text}: {value} {text === 'Positive' ? '%' : ''}</p>
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}{text === 'Positive' ? '%' : ''}</td>
+    </tr>
+  )
 }
 
 const App = () => {
@@ -34,14 +41,6 @@ const App = () => {
   const all = good + bad + neutral
   const avg = (good - bad) / all || 0
   const percent = (good / all) * 100 || 0
-  const values = {
-    good: good,
-    neutral: neutral,
-    bad: bad,
-    all: all,
-    avg: avg,
-    percent: percent
-  }
 
   return (
     <div>
@@ -50,7 +49,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral'/>
       <Button handleClick={() => setBad(bad + 1)} text='bad'/>
       <Header text='statistics' />
-      <Statistics values={values} />
+      <Statistics { ...{ good, neutral, bad, all, avg, percent } } />
     </div>
   )
 }
